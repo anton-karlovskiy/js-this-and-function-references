a59b6fe71bd44de5489424f1ef56a6dfaf43be6b
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-07-23 10:40:24 +0300

    [feat] implemented the code behavior

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
+
 class NameField {
-    constructor(name) {
-        const field = document.createElement('li');
-        field.textContent = name;
-        const nameListHook = document.querySelector('#names');
-        nameListHook.appendChild(field);
-    }
+  constructor(name) {
+    const field = document.createElement('li');
+    field.textContent = name;
+    const nameListHook = document.querySelector('#names');
+    nameListHook.appendChild(field);
+  }
 }
 
 class NameGenerator {
-    constructor() {
-        const btn = document.querySelector('button');
-        btn.addEventListener('click', ???);
-    }
+  constructor() {
+    const btn = document.querySelector('button');
+    this.names = ['Max', 'Manu', 'Anna'];
+    this.currentName = 0;
+    // btn.addEventListener('click', this.addName); // MEMO: outputs [NameGenerator addName] this =>  <button>​Add Name​</button>​
+    // btn.addEventListener('click', this.addName.bind(this)); // MEMO: outputs [NameGenerator addName] this =>  NameGenerator {names: Array(3), currentName: 0}
+    btn.addEventListener('click', () => {
+      this.addName(); // MEMO: outputs [NameGenerator addName] this =>  NameGenerator {names: Array(3), currentName: 0}
+    });
+  }
 
-    addName() {
-        const name = new NameField("Max");
+  addName() {
+    console.log('[NameGenerator addName] this => ', this);
+    const name = new NameField(this.names[this.currentName]);
+    this.currentName++;
+    if (this.currentName >= this.names.length) {
+      this.currentName = 0;
     }
+  }
 }
 
 const gen = new NameGenerator();
